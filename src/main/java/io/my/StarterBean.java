3c2ea191b9c9c604b2e2d5f10edca4a92bcaab5b
feiyanke <feiyanke@126.com>
2020-01-04 20:14:25 +0800

    add dirty version and conditionalbean

--- src/main/java/io/my/StarterBean.java
+++ src/main/java/io/my/StarterBean.java
@@ -1,13 +1,14 @@
 package io.my;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class StarterBean {
 
-    @Autowired
-    private StarterProperties properties;
+    private final StarterProperties properties;
 
     public void func1() {
         System.out.println("111 StarterBean func1() called: " + properties);
--- src/main/java/io/my/StarterProperties.java
+++ src/main/java/io/my/StarterProperties.java
@@ -1,9 +1,12 @@
 package io.my;
 
 import lombok.Data;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @Data
+@ConditionalOnMissingBean
 @ConfigurationProperties(prefix = "starter")
 public class StarterProperties {
     int property0 = 0;
